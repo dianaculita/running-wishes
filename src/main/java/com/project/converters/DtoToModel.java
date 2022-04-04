8754bb1dfd11cdefc74a2bbedd9c60fbcb6eca4c
mdian <mdiana464@gmail.com>
2022-04-04 08:22:35 +0300

    removed unused methods

--- src/main/java/com/project/converters/DtoToModel.java
+++ src/main/java/com/project/converters/DtoToModel.java
@@ -1,8 +1,13 @@
 package com.project.converters;
 
-import com.project.dtos.*;
-import com.project.models.*;
+import com.project.dtos.AssociationDto;
+import com.project.dtos.SportDto;
+import com.project.models.Association;
+import com.project.models.Sport;
 
+/**
+ * Util class for converting a dto object to an entity model object
+ */
 public class DtoToModel {
 
     public static Association fromAssociationDto(AssociationDto associationDto) {
@@ -12,54 +17,11 @@ public class DtoToModel {
                 .build();
     }
 
-    public static CharityPerson fromCharityPersonDto(CharityPersonDto charityPersonDto) {
-        return CharityPerson.builder()
-                .personCnp(charityPersonDto.getPersonCnp())
-                .iban(charityPersonDto.getIban())
-                .story(charityPersonDto.getStory())
-                .neededFund(charityPersonDto.getNeededFund())
-                .raisedFund(charityPersonDto.getRaisedFund())
-                .build();
-    }
-
-    public static Sponsor fromSponsorDto(SponsorDto sponsorDto) {
-        return Sponsor.builder()
-                .sponsorId(sponsorDto.getSponsorId())
-                .name(sponsorDto.getName())
-                .build();
-    }
-
-    public static Participant fromParticipantDto(ParticipantDto participantDto) {
-        return Participant.builder()
-                .cnp(participantDto.getCnp())
-                .name(participantDto.getName())
-                .build();
-    }
-
     public static Sport fromSportDto(SportDto sportDto) {
         return Sport.builder()
                 .sportId(sportDto.getSportId())
                 .name(sportDto.getName())
                 .build();
     }
-
-    public static Competition fromCompetitionDto(CompetitionDto competitionDto) {
-        return Competition.builder()
-                .competitionId(competitionDto.getCompetitionId())
-                .name(competitionDto.getName())
-                .location(competitionDto.getLocation())
-                .numberOfDays(competitionDto.getNumberOfDays())
-                .ticketFee(competitionDto.getTicketFee())
-                .raisedMoney(competitionDto.getRaisedMoney())
-                .build();
-    }
-
-    public static Donation fromDonationDto(DonationDto donationDto) {
-        return Donation.builder()
-                .donationId(donationDto.getDonationId())
-                .totalFunds(donationDto.getTotalFunds())
-                .build();
-    }
-
 
 }
